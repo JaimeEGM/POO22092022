2ee65ceb3b43b6657333fe1a7ac3dc15cb3359c8
JaimeEGM <jaimegonzalez006@aragon.unam.mx>
2022-03-29 12:49:08 -0600

    Clase: PruebasPolimorfismo

--- clases/ReutilizacionCodigo2209/src/ico/fes/herencia/Alumno.java
+++ clases/ReutilizacionCodigo2209/src/ico/fes/herencia/Alumno.java
@@ -47,9 +47,22 @@ public class Alumno extends Persona {
     public String toString() {
         return super.toString() + "Alumno{" + "numeroCuenta=" + numeroCuenta + ", carrera=" + carrera + '}';
     }
-    
-    public void estudiar(int cantidad){
+
+    public void estudiar(int cantidad) {
         System.out.println(this.getNombre() + "está estudiando" + cantidad + "Hrs.");
+    }
+   // Polimorfismo por sobre escritura
+    @Override
+    public void dormir() {
+        System.out.println("El estudiante "
+                + this.getNombre() + " está durmiendo 5 horas...");
+    }
+    
+    // Polimorfismo por sobre carga
+    public void dormir(int horasEstudio) {
+        int horasDormir = 8;
+        System.out.println(this.getNombre()
+                + " está durmiendo " + (horasDormir - horasEstudio) + " Horas ");
     }
 
 }
