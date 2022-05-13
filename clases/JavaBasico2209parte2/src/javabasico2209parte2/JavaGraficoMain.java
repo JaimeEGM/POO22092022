a8986e1537565dc0e5de94c4ae148a4db1c0203d
JaimeEGM <jaimegonzalez006@aragon.unam.mx>
2022-05-12 21:58:09 -0500

    Clase 12/05/2022

---
+++ clases/JavaBasico2209parte2/src/javabasico2209parte2/JavaGraficoMain.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javabasico2209parte2;
+
+import ico.fes.awt.Ventana;
+import ico.fes.awt.VentanaVersion2;
+import ico.fes.swing.VentanaSwing;
+
+/**
+ *
+ * @author jaimegm
+ */
+public class JavaGraficoMain {
+    
+    //Ventana ventana=new Ventana("Hola AWT ICO FES Aragón");
+        
+        //VentanaVersion2 ventana = new VentanaVersion2();
+        
+        VentanaSwing vs=new VentanaSwing();
+        System.out.println(System.getProperty("user.dir"));
+    }
+}
